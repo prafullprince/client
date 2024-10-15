ca8cd7835df71efeb7117a5a204f26358c80ebcf
prafullprince <prafullprince12@gmail.com>
2024-10-16 03:28:36 +0530

    spotlight

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Spotlight } from '../components/ui/Spotlight'
 
 function Homepage() {
   return (
-    <div className='text-white'>
+    <div className='text-white relative min-h-screen'>
       <div className='w-[90%] mx-auto md:w-[80%]'>
-        Home page
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="white"
+        />
       </div>
     </div>
   )
