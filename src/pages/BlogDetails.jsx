62d7dcebeedcc5141fb8004bfd111b50e57c73a3
prafullprince <prafullprince12@gmail.com>
2024-10-21 03:06:11 +0530

    blogPageDeatils complete

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { fetchAllBlogsDeatils } from "../service/apiCall/courseApiCall";
 import { IoTimeOutline } from "react-icons/io5";
-import { WobbleCard } from "../components/ui/wobble-card";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import { ShootingStars } from "../components/ui/shooting-stars";
+import { StarsBackground } from "../components/ui/stars-background";
+
 
 function BlogDetails() {
   const { blogId } = useParams();
@@ -22,7 +25,7 @@ function BlogDetails() {
   console.log(blogDetails?.thumbnail);
 
   return (
-    <div className="text-white relative">
+    <div className="text-white relative pb-12">
       {/* topbar */}
       <div className="bg-[#161D29] px-1 py-12 font-inter relative">
         <div className="w-[90%] md:w-[80%] mx-auto flex flex-col gap-6 lg:flex-row lg:gap-2 lg:justify-between">
@@ -62,23 +65,59 @@ function BlogDetails() {
         </div>
       </div>
       {/* content */}
-      <div className="w-[90%] md:w-[80%] mx-auto flex flex-col gap-8">
+      <div className="w-[90%] md:w-[80%] mx-auto flex flex-col gap-8 relative z-10">
         {/* What you will Learn */}
-        <div className="md:px-12 md:py-8 px-2 py-1 mt-16 border border-[#2C333F] w-fit shadow-sm shadow-blue-25">
+        <div className="md:px-12 md:py-8 px-2 py-1 mt-16 border border-[#2C333F] w-fit shadow-sm shadow-blue-25 md:w-[60%] relative z-20">
           <div className="text-[#F1F2FF] font-medium text-3xl">
             What You'll learn
           </div>
           <div className="mt-3 flex flex-col gap-2">
-            {blogDetails?.blogContent?.map((section) => (
-              <p key={section?._id} className="font-medium text-[#C5C7D4]">
-                {section.name.substring(0, 40)}...
-              </p>
+            {blogDetails?.blogContent?.map((section,index) => (
+              <Link key={section?._id} to={`#${index}`} className="font-medium text-[#C5C7D4]">
+                {index+1} {"."} {" "} {section.name.substring(0, 40)}...
+              </Link>
             ))}
           </div>
         </div>
         {/* Blog Content */}
+        <div className="mt-12 flex flex-col gap-8 relative z-20">
+            <div className="flex flex-col gap-3">
+              <div className="text-white font-semibold text-2xl">Course Content</div>
+              <div className="text-[#C5C7D4] text-sm flex gap-2">
+                10 sections
+              </div>
+            </div>
+            {/* nested content */}
+            <div className="">
+              {
+                blogDetails?.blogContent?.map((section,index)=>(
+                  <details className="" key={section?._id}>
+                     <summary className="list-none lg:w-[60%] flex justify-between cursor-pointer border border-[#424854] bg-[#2C333F] text-[#F1F2FF] font-medium px-4 py-3 transition-all duration-200 z-40">
+                      <div className="flex items-center gap-2 w-full">
+                        <MdOutlineArrowDropDown className="text-xl" />
+                        <div className="break-words w-[80%]">{section?.name.substring(0,80)}</div>
+                      </div>
+                      <div></div>
+                     </summary>
+                     <div>
+                        {
+                          section?.subSection?.map((subSec,index)=>(
+                            <div className="py-2 flex flex-col w-full" key={subSec._id}>
+                                <img className="rounded-lg shadow-md drop-shadow-lg shadow-caribbeangreen-200 lg:w-[59%] ml-2 h-[350px] aspect-auto" src={subSec?.imageUrl} />
+                                <div className="lg:w-[100%] break-words py-2 relative">
+                                  <p className="px-4 mt-3 lg:w-[60%] text-clip text-wrap text-[#C5C7D4]">{`->  `}{subSec?.body}</p>
+                                </div>
+                            </div>
+                          ))
+                        }
+                     </div>
+                  </details>
+                ))
+              }
+            </div>
+        </div>
         {/* author */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 mt-8 relative z-20">
             <p className="text-[#F1F2FF] font-semibold text-2xl shadow-sm shadow-blue-100 w-fit p-2">Author</p>
             <div className="flex gap-2 items-center">
               <img 
@@ -91,6 +130,8 @@ function BlogDetails() {
               <p className="text-[#C5C7D4] text-sm">{blogDetails?.description}</p>
             </div>
         </div>
+        <ShootingStars />
+        <StarsBackground />
       </div>
     </div>
   );
