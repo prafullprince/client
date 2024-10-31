00547cf81b94be1da3d98ded448dd0750ce593e6
prafullprince <prafullprince12@gmail.com>
2024-10-31 14:21:12 +0530

    modalSize according to mobile

--- src/components/auth/ProfileDropDownConModal.jsx
+++ src/components/auth/ProfileDropDownConModal.jsx
@@ -19,7 +19,7 @@ function ProfileDropDownConModal({ modalData, setModalData }) {
 
   return (
     <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
-      <div className="w-11/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6">
+      <div className="w-11/12 max-w-[330px] rounded-lg border border-richblack-400 bg-richblack-800 p-6">
         <div>
           <div className="flex justify-between">
             <p className="text-2xl font-semibold text-richblack-5">
